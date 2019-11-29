accbc493f51f5d46b68fe7f44855e7c0c062e7f0
nskene <nathan.skene@gmail.com>
2019-11-29 01:02:56 +0000

    load_genelist wasn't being exported

--- R/load_genelist.r
+++ R/load_genelist.r
@@ -1,3 +1,17 @@
+#' load_genelist
+#'
+#' \code{load_genelist} Loads a gene list from a file
+#' 
+#' Expects the first line to be descriptive
+#'
+#' @param path Folder where it is stored
+#' @param lookuptable Dataframe containing HGNC.symbol and MGI.symbol as columns
+#' @param speciesWanted Either 'human' or 'mouse'
+#' @return res Results
+#' @examples
+#' # list_path = sprintf("%s/%s.txt",path,listN)
+#' # geneListHGNC = load.genelist(list_path,orthologs,speciesWanted="human")
+#' @export
 load_genelist <- function(path,lookuptable,speciesWanted="human"){
     # Check speciesWanted is either human or mouse
     if(sum(speciesWanted %in% c("human","mouse"))!=1){

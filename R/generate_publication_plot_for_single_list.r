5eb87a0a7d0b2190c50120129fe12f8c609b1f11
nskene <nathan.skene@gmail.com>
2019-11-29 02:15:50 +0000

    Adjusted plotting settings

--- R/generate_publication_plot_for_single_list.r
+++ R/generate_publication_plot_for_single_list.r
@@ -5,13 +5,16 @@
 #' @param allRes Output of test_all_comparisons
 #' @param listName Used to select a particular list from listName (not relevant to current version of package)
 #' @param save_folder Folder where output PDFs should be stored
+#' @param angle for xaxis labels, either 90 or 45 is probably best
 #' @return thePlot (as a ggplot)
 #' @examples
 #' allDataSets = list(mouseSS_dist,tasic_mouse_dist,dronc_mouse_dist,dronc_human_dist,divseq_dist)
 #' res = test_all_comparisons(allDataSets,orthologs,sharedName="Pyramidal Neuron")
 #' thePlot = generate_publication_plot_for_single_list(res)
 #' @export
-generate_publication_plot_for_single_list <- function(allRes,listName="dendrite_enriched_transcripts_HGNC_1to1only",save_folder=""){
+#' @import cowplot
+#' @import ggplot2
+generate_publication_plot_for_single_list <- function(allRes,listName="dendrite_enriched_transcripts_HGNC_1to1only",save_folder="",xaxis_angle=90){
     #load("~/Box Sync/DendriticEnrichment/allRes_LotsOfListsWtP.Rda")
     if(!is.na(listName)){
         subRes = allRes[allRes$list == listName,]
@@ -28,8 +31,9 @@ generate_publication_plot_for_single_list <- function(allRes,listName="dendrite_
     thePlot = ggplot(subRes)+geom_bar(aes(x=labels,y=z,fill=signif),stat="identity",position="dodge", width=0.9)+
         #facet_wrap(~groupLabels, strip.position = "bottom", scales = "free_x")+
         #facet_grid(.~groupLabels, scales = "free", space = "free", switch="x")+
+        theme_cowplot()+
         facet_grid(list~groupLabels, scales = "free", space = "free", switch="x")+
-        theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+        theme(axis.text.x = element_text(angle = 90, hjust = 1))+
         ylab("Z-Score")+xlab("")+
         scale_fill_manual(values=group.colors)+
         scale_y_continuous(breaks=seq(-20,20,1)) + guides(fill=FALSE)
